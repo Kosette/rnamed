d2e066da53447d5beb13db25694f09040209d7f5
Delphi Lin <35268640+Kosette@users.noreply.github.com>
2023-10-30 05:52:36 +0800

    refactor: refactor codes

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,31 @@
-use blake3::Hasher;
 use glob::glob;
 use rayon::prelude::*;
+use rnamed::rnamed;
 use std::env;
-use std::fs;
-use std::path::{Path, PathBuf};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("No Path Found!");
+        return;
+    }
+
+    if args.contains(&String::from("--help")) {
+        println!("Usage: rnamed <Path...>");
+        println!("    Tip: multiple paths and globs accepted.");
+        println!("    Warning: filename containing special characters like [] may be ignored.");
+        return;
+    }
 
     args[1..].par_iter().for_each(|pattern| {
         for entry in glob(pattern).expect("Failed to read glob pattern") {
             let path = entry.unwrap();
             if path.is_dir() {
-                process_dir(&path);
+                rnamed::process_dir(&path);
             } else if path.is_file() {
-                rename_file(&path);
+                rnamed::rename_file(&path);
             }
         }
     });
-}
-
-fn process_dir(dir: &Path) {
-    match fs::read_dir(dir) {
-        Ok(paths) => paths.into_iter().filter_map(|e| e.ok()).for_each(|e| {
-            let path = e.path();
-            if path.is_dir() {
-                process_dir(&path);
-            } else {
-                rename_file(&path);
-            }
-        }),
-        Err(err) => {
-            eprintln!("Error reading dir {:?}: {}", dir, err);
-        }
-    }
-}
-
-fn rename_file(path: &Path) {
-    let new_name = get_new_name(path);
-    if new_name.exists() {
-        println!("File {} already exists, skipping", new_name.display());
-    } else {
-        fs::rename(path, &new_name).unwrap();
-    }
-}
-
-fn get_new_name(path: &Path) -> PathBuf {
-    let ext = path.extension().and_then(|e| e.to_str()).unwrap_or("");
-    let name = format!("{}.{}", get_blake3(path).to_uppercase(), ext);
-    path.with_file_name(name)
-}
-
-fn get_blake3(path: &Path) -> String {
-    let data = fs::read(path).unwrap();
-    let mut hasher = Hasher::new();
-    hasher.update(&data);
-    let hash = hasher.finalize();
-    format!("{}", hash)
 }
